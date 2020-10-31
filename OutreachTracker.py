a62bfb8faeca996923d3cfbf8b32eadd41089b3c
Arjun Karanam <akaranam@stanford.edu>
2020-10-31 02:01:46 -0400

    Update OutreachTracker.py

--- OutreachTracker.py
+++ OutreachTracker.py
@@ -1,14 +1,22 @@
+#!/usr/bin/env python
+# coding: utf-8
 
 import csv
 
 #Fields to modify
-inputFile = 'lvf_ballot_chase_01_messages.csv' #Change to name of input file
+inputFile = 'lvf_early_vote_02_messages.csv' #Change to name of input file
 outputFile = 'output.csv'
+beginningTime = "17"
+endTime = "20"
 
 #In the Data Dictionary, the email is stored as the key, and the [message, date] is stored as the value
 dataDict = {}
 countDict = {} #{date: {email: count}}
 finalOutput = []
+
+
+# In[48]:
+
 
 def readCSV():
     with open(inputFile) as csv_file:
@@ -20,18 +28,22 @@ def readCSV():
         #Looping through the CSV, storing messages as {date: [(email, message), (email, message)]}
         for row in csv_reader:
             currDate = parseDate(row[0])
-            if (row[7] != ""):
-                #row7 = email, row3 = message, row[4] = first name, row[5] = last name
-                currEntry = (row[7], row[4], row[5])
-                if currDate in dataDict:
-                    dataDict[currDate].append(currEntry)
-                else:
-                    dataDict[currDate] = [currEntry]   
-
+            currTime = int(getTime(row[0]))
+            if currTime >= int(beginningTime) and currTime <= int(endTime):
+                if (row[7] != ""):
+                    #row7 = email, row3 = message, row[4] = first name, row[5] = last name
+                    currEntry = (row[7], row[4], row[5])
+                    if currDate in dataDict:
+                        dataDict[currDate].append(currEntry)
+                    else:
+                        dataDict[currDate] = [currEntry]   
 
 #Converting '2020-09-25 20:33:49.979033Z' to '09-25'
 def parseDate(date):
     return (date[5:10])
+
+def getTime(date):
+    return date[11:13]
 
 #Fetching the number of emails per user per date
 def count():
@@ -49,11 +61,11 @@ def createOutput():
     for date in countDict:
         for entry in countDict[date]:
             finalOutput.append([date, entry, countDict[date][entry]["firstName"], countDict[date][entry]["lastName"], countDict[date][entry]["count"]])
-        
+
+fields = ['Date', 'E-mail', "First Name", "Last Name", 'Number of Messages'] 
 
 def writeToCSV():
     with open(outputFile, 'w') as csvfile:  # Just use 'w' mode in 3.x
-        fields = ['Date', 'E-mail', "First Name", "Last Name", 'Number of Messages']
         # creating a csv writer object  
         csvwriter = csv.writer(csvfile)  
 
@@ -70,4 +82,3 @@ def mainFunc():
     writeToCSV()
 
 mainFunc()
-
